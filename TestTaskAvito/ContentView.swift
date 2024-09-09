e8af26e7320ddb109e41dd69556a00eedd7ba1c2
denysooz <den.darevskiy@mail.ru>
2024-09-09 16:17:04 +0300

    Initial Commit

---
+++ TestTaskAvito/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTaskAvito
+//
+//  Created by Denis Dareuskiy on 9.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTaskAvito/TestTaskAvitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskAvitoApp.swift
+//  TestTaskAvito
+//
+//  Created by Denis Dareuskiy on 9.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskAvitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
